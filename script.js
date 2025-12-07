8cb8802e759c7f8911d38842a5d03673f734400e
Amirhossein Jamalian <amirjamal2023g@gmail.com>
2025-12-07 16:30:02 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("keydown", (event) => {
+  document.getElementById(event.code).classList.add("pressed");
+});
+
+document.addEventListener("keyup", (event) => {
+  document.getElementById(event.code).classList.remove("pressed");
+});
